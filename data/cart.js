b00ee02def75dbb3f92e2a2219146535acaf247b
kajeme11 <chenequek@gmail.com>
2024-12-26 21:54:40 -0800

    checkout delivery options rendering

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,18 @@
+import { products } from "./products.js";
+
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
     cart = [
         {
             productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2
+            quantity: 2,
+            deliveryOptionId: '1'
         },
         {
             productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: '2'
         }
     ];
 }
@@ -32,7 +36,8 @@ export function addToCart(productId){
         cart.push( 
             {
                 productId: productId,
-                quantity: 1
+                quantity: 1,
+                deliveryOptionId: '1'
             }
         );
     }
@@ -66,4 +71,16 @@ export function removeProductFromCart(productId){
     }
     saveToStorage();
     console.log(cart);
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+    let matchingProduct;
+    cart.forEach((cartItem) => {
+        if(cartItem.productId === productId){
+            matchingProduct = cartItem;
+        }
+        console.log("delivery option " + deliveryOptionId)
+    });
+    matchingProduct.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
 }
