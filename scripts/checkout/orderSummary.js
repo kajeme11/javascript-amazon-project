2e03a03ea6286954c8118fb5174bc504b0651e40
kajeme11 <chenequek@gmail.com>
2024-12-26 23:16:56 -0800

    re render updates at checkout

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from "../../data/products.js";
 import {formatCurrency} from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 console.log(dayjs());
 const today = dayjs();
@@ -122,6 +123,7 @@ export function renderOrderSummary(){
             removeProductFromCart(productId);  
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             console.log(container);
+            renderPaymentSummary();
         });
     })
     // console.log(cartSummaryHTML);
@@ -130,6 +132,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId)
             renderOrderSummary();
+            renderPaymentSummary();
         })
     })
 }
