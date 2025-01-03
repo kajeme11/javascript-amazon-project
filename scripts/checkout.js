d174b501d96cd297184aee764ec982739a329026
kajeme11 <chenequek@gmail.com>
2025-01-02 21:33:04 -0800

    modified checkout js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,19 @@ import {renderOrderSummary} from './checkout/orderSummary.js'
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
 
-loadProducts(() => {
+
+new Promise((resolve) => {
+    loadProducts(() => {
+        console.log('Products loaded');
+        resolve();
+    })
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
+    
 });
+
+// loadProducts(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
